b2c59cd6b13650f5fa6866d06411d9b4fc2d8ef8
Samuel Dietrick <samueldietrick@hotmail.com>
2024-01-15 14:10:57 -0300

    Create ring-size.php

---
+++ ring-size.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name: Ring Size
+ * Description: O plugin Ring Size for WooCommerce é uma solução inovadora projetada para auxiliar clientes na medida precisa de anéis e alianças ao fazer compras em sua loja online WooCommerce. Elimine as preocupações relacionadas ao tamanho incorreto, proporcionando uma experiência de compra mais confiável e personalizada.
+ * Version: 1.0
+ * Author: Samuel Dietrick
+ */
+
+
+function saudacao_shortcode() {
+    return '<p>Base do projeto em andamento</p>';
+}
+
+add_shortcode('saudacao', 'saudacao_shortcode');
+?>
