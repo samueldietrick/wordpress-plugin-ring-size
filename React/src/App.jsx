0782982b894213e7f56ff7b24b1edda6aa838c65
Samuel Dietrick <hello@samueldietrick.com>
2024-01-14 23:08:04 -0300

    first commit
    first version of the code

---
+++ React/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import AllianceSizer from './component/AllianceSizer/AllianceSizer'
+
+function App() {
+
+  return (
+    <>
+      <AllianceSizer />
+    </>
+  )
+}
+
+export default App
